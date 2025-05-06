8993798d85435f3f8bd09891c91b9f3815716a9a
itlattice <zqu1016@qq.com>
2025-05-06 13:40:06 +0800

    init:项目提交

---
+++ src/Client.php
@@ -0,0 +1,20 @@
+<?php
+namespace iboxs\IM;
+
+/**
+ * @see \iboxs\IM\lib\Base
+ * @package iboxs\IM
+ * @mixin \iboxs\IM\lib\Base
+ * @version 2.0
+ * @author ITLattice https://github.com/itlattice https://gitee.com/gz8 联系QQ：320587491 微信：itlattices
+ * @license MIT
+ * @method static void alipayWebpay(string $no,float $amount,string $orderName) 支付宝网页支付（含手机端和PC端）[直接跳转]
+ */
+class Client
+{
+    public static function install(){
+        if(function_exists('root_path')){
+            
+        }
+    }
+}
